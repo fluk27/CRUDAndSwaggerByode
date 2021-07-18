d3889d393dfe544aff5ef57caaad98be27dabe92
unknown <wongsathon27@gmail.com>
2021-07-19 01:15:51 +0700

    add mockdata

--- index.js
+++ index.js
@@ -16,4 +16,30 @@ app.post('/user/login',login)
 app.post('/user/register',register)
 app.use(authen)
 app.use(route)
+
+app.get('/mockData', (req, res) => {
+  const mockData ={
+    payeeProxyId : "612698448287644",
+payeeProxyType : "BILLERID" ,
+payeeAccountNumber : "0987654321",
+payeeName: "Artani Bill Payment",
+payerProxyId: "6380760001",
+payerProxyType: "ACCOUNT" ,
+payerAccountNumber: "6380760001",
+payerName: "Sutunpan Jumcom",
+sendingBankCode: "014",
+receivingBankCode: "014" ,
+amount: "750.45" ,
+channelCode: "PMH",
+transactionId: "202107127J00F00bKTPBpeh",
+transactionDateandTime: "2021-07-12T15:14:42+07:00",
+billPaymentRef1: "PORTALSANDBOXREF1",
+billPaymentRef2: "PORTALSANDBOXREF1",
+billPaymentRef3: "SCB",
+currencyCode: "764" ,
+transactionType : "Domestic Transfer"
+  }
+  res.status(200).json(mockData)
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
